d7f02d46830db8a3322e65177eb2cf139f5e0e2e
Hridoy Ahmed <hridoyahmed47@gmail.com>
2025-04-19 14:22:37 +0600

    some typography changed with tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,10 @@ import Home from "./ui/Home";
 import Error from "./ui/Error";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
-import CreateOrder, {action as createOrderAction} from "./features/order/CreateOrder";
-import Order, {loader as orderLoader} from "./features/order/Order";
+import CreateOrder, {
+  action as createOrderAction,
+} from "./features/order/CreateOrder";
+import Order, { loader as orderLoader } from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
 
 const router = createBrowserRouter([
@@ -43,8 +45,6 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-  const we = 23;
-
   return <RouterProvider router={router} />;
 }
 
--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold">Hridoy</div>;
+  return <div className="hidden text-sm font-semibold">Hridoy</div>;
 }
 
 export default Username;
