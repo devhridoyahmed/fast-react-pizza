3f7e9d2d80247ea8d4e32e35debcde6a23b81606
Hridoy Ahmed <hridoyahmed47@gmail.com>
2025-04-20 06:13:07 +0600

    used tailwind css flex classes for layout

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Hridoy</div>;
+  return <div className="hidden text-sm font-semibold md:block">Hridoy</div>;
 }
 
 export default Username;
